262d9548101a18165c0297d34b79ee27c727d0dd
DanielSchripsema <73393540+DanielSchripsema@users.noreply.github.com>
2024-03-27 18:06:40 +0100

    test

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
 const router = express.Router();
+const { db } = require('../services/database');
 
-/* GET home page. */
-router.get('/', (req, res) => {
+/* GET users listing. */
+router.get('/', async (req, res) => {
   res.render('index', { title: 'Express' });
+  const users = await db.collection('users').find().toArray();
+  res.json(users);
 });
+// /* GET home page. */
+// router.get('/', (req, res) => {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
