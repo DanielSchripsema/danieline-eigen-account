fcd11e8c2f9011293f32ed918c4ab3ffa1986b8d
DanielSchripsema <73393540+DanielSchripsema@users.noreply.github.com>
2024-04-02 18:29:21 +0200

    Revert "test"
    This reverts commit 262d9548101a18165c0297d34b79ee27c727d0dd.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-const { db } = require('../services/database');
 
-/* GET users listing. */
-router.get('/', async (req, res) => {
+/* GET home page. */
+router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
-  const users = await db.collection('users').find().toArray();
-  res.json(users);
 });
-// /* GET home page. */
-// router.get('/', (req, res) => {
-//   res.render('index', { title: 'Express' });
-// });
 
 module.exports = router;
